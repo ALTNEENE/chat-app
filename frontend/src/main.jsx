10f664f487d5507e6e55ca593650ca86806866a4
M. A. Yahia <109676930+ALTNEENE@users.noreply.github.com>
2024-06-25 22:41:33 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import { AuthContextProvider } from './context/useAuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename={import.meta.env.BASE_URL}>
         <AuthContextProvider>
           <App />
         </AuthContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
