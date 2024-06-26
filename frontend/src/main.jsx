67830f0ba665cae89625755bf6c385a7a682b446
M. A. Yahia <109676930+ALTNEENE@users.noreply.github.com>
2024-06-26 22:54:40 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import { AuthContextProvider } from './context/useAuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="chat-app">
         <AuthContextProvider>
           <App />
         </AuthContextProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
