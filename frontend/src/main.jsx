111bd63811977c45e91c069f612194d6f44ea4d6
M. A. Yahia <109676930+ALTNEENE@users.noreply.github.com>
2024-06-26 22:55:28 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { AuthContextProvider } from './context/useAuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="chat-app">
+    <BrowserRouter basename="/chat-app">
         <AuthContextProvider>
           <App />
         </AuthContextProvider>
